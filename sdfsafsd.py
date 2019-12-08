54dfac6fdb6eb4cc08746c663a4241c76a8c4461
Kimhwiwoong <khw45829811@gmail.com>
2019-12-08 17:41:16 +0900

    useless py file deleted

--- sdfsafsd.py
+++
@@ -1,8 +0,0 @@
-from operator import itemgetter
-TOPFIVE=[('kim',0),('kim',0),('kim',0),('kim',0),('kim',0)]
-TOPFIVE.append(('lee',1))
-TOPFIVE.sort(key=itemgetter(1),reverse=True)
-TOPFIVE.pop()
-print(TOPFIVE)
-if(TOPFIVE[4][1] < 100):
-    print("!!")
